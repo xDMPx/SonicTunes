e8ee9ae11abeaefd78f259a89748b6d591219fb2
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-12-16 14:20:17 +0100

    Implement basic command mode

--- src/tui.rs
+++ src/tui.rs
@@ -6,7 +6,7 @@ use ratatui::{
     widgets::{Block, Borders},
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TuiCommand {
     State(TuiState),
     Quit,
@@ -16,6 +16,34 @@ pub enum TuiCommand {
     PlayNext,
     PlayPrevious,
     Scroll(i16),
+    EnterCommandMode(bool),
+}
+
+fn map_str_to_tuicommand(str: &str) -> Option<TuiCommand> {
+    if str.split_whitespace().count() > 2 {
+        return None;
+    }
+
+    let mut tokens = str.split_whitespace();
+    let command_str = tokens.next()?;
+    let mut args = tokens;
+
+    match command_str {
+        "quit" | "q" => Some(TuiCommand::Quit),
+        "vol" => {
+            let mut volume: i64 = args.next()?.parse().ok()?;
+            volume = volume.clamp(-200, 200);
+            Some(TuiCommand::Volume(volume))
+        }
+        "seek" => {
+            let offset: f64 = args.next()?.parse().ok()?;
+            Some(TuiCommand::Seek(offset))
+        }
+        "play-pause" => Some(TuiCommand::PlayPause),
+        "play-next" => Some(TuiCommand::PlayNext),
+        "play-prev" => Some(TuiCommand::PlayPrevious),
+        _ => None,
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -28,6 +56,9 @@ pub fn tui(
     libmpv_s: crossbeam::channel::Sender<LibMpvMessage>,
     tui_r: crossbeam::channel::Receiver<LibMpvEventMessage>,
 ) -> Result<(), SonicTunesError> {
+    let mut command_mode = false;
+    let mut command_text = "".to_string();
+
     let commands = std::collections::HashMap::from([
         (
             KeyEvent::new(KeyCode::Char('1'), KeyModifiers::NONE),
@@ -93,6 +124,14 @@ pub fn tui(
             KeyEvent::new(KeyCode::Char('k'), KeyModifiers::NONE),
             TuiCommand::Scroll(-1),
         ),
+        (
+            KeyEvent::new(KeyCode::Char(':'), KeyModifiers::NONE),
+            TuiCommand::EnterCommandMode(true),
+        ),
+        (
+            KeyEvent::new(KeyCode::Esc, KeyModifiers::NONE),
+            TuiCommand::EnterCommandMode(false),
+        ),
     ]);
     let mut tui_state = TuiState::Player;
 
@@ -144,7 +183,16 @@ pub fn tui(
                     secs_to_hms(playback_duration),
                     playback_volume
                 ));
-                draw(&mut terminal, &to_draw, 0)?;
+                draw(
+                    &mut terminal,
+                    &to_draw,
+                    0,
+                    if command_mode {
+                        Some(&command_text)
+                    } else {
+                        None
+                    },
+                )?;
             }
             TuiState::History => {
                 let mut to_draw = "".to_string();
@@ -155,7 +203,16 @@ pub fn tui(
                     };
                     to_draw.push_str(&format!("{x}\n"))
                 });
-                draw(&mut terminal, &to_draw, scroll)?;
+                draw(
+                    &mut terminal,
+                    &to_draw,
+                    scroll,
+                    if command_mode {
+                        Some(&command_text)
+                    } else {
+                        None
+                    },
+                )?;
             }
         };
 
@@ -163,8 +220,32 @@ pub fn tui(
             let event = event::read();
             if let Ok(event) = event {
                 log::debug!("Event: {event:?}");
+                let mut command = None;
                 if let event::Event::Key(key) = event {
-                    if let Some(command) = commands.get(&key) {
+                    if command_mode {
+                        if key.code.to_string().len() == 1 {
+                            let c = key.code.to_string().chars().next().unwrap();
+                            if c.is_alphanumeric() || c == '-' {
+                                command_text.push(c);
+                            }
+                        } else if key.code == event::KeyCode::Backspace {
+                            let _ = command_text.pop();
+                        } else if key.code == event::KeyCode::Esc {
+                            command_mode = false;
+                            command_text = "".to_string();
+                        } else if key.code == event::KeyCode::Enter {
+                            command = map_str_to_tuicommand(&command_text);
+                            command_mode = false;
+                            command_text = "".to_string();
+                        } else if key.code == event::KeyCode::Char(' ') {
+                            command_text.push(' ');
+                        }
+                    } else {
+                        if let Some(key_command) = commands.get(&key) {
+                            command = Some(key_command.clone());
+                        }
+                    }
+                    if let Some(command) = command {
                         log::debug!("Command: {command:?}");
                         match command {
                             TuiCommand::State(state) => {
@@ -175,10 +256,10 @@ pub fn tui(
                                 break;
                             }
                             TuiCommand::Volume(vol) => {
-                                libmpv_s.send(LibMpvMessage::UpdateVolume(*vol))?;
+                                libmpv_s.send(LibMpvMessage::UpdateVolume(vol))?;
                             }
                             TuiCommand::Seek(offset) => {
-                                libmpv_s.send(LibMpvMessage::UpdatePosition(*offset))?;
+                                libmpv_s.send(LibMpvMessage::UpdatePosition(offset))?;
                             }
                             TuiCommand::PlayPause => {
                                 libmpv_s.send(LibMpvMessage::PlayPause)?;
@@ -190,12 +271,14 @@ pub fn tui(
                                 libmpv_s.send(LibMpvMessage::PlayPrevious)?;
                             }
                             TuiCommand::Scroll(x) => {
-                                let x = *x;
                                 if x > 0 && scroll < (history.len() - 1) as u16 {
                                     scroll += 1;
                                 } else if x < 0 && scroll > 0 {
                                     scroll -= 1;
                                 }
+                            }
+                            TuiCommand::EnterCommandMode(enter) => {
+                                command_mode = enter;
                             }
                         }
                     }
@@ -262,7 +345,12 @@ pub fn tui(
     Ok(())
 }
 
-pub fn draw(terminal: &mut DefaultTerminal, text: &str, scroll: u16) -> Result<(), std::io::Error> {
+pub fn draw(
+    terminal: &mut DefaultTerminal,
+    text: &str,
+    scroll: u16,
+    command: Option<&str>,
+) -> Result<(), std::io::Error> {
     terminal.draw(|f| {
         let area = f.area();
         let block = Block::default()
@@ -274,6 +362,13 @@ pub fn draw(terminal: &mut DefaultTerminal, text: &str, scroll: u16) -> Result<(
         let inner = block.inner(f.area());
         f.render_widget(block, area);
         f.render_widget(text, inner);
+        if let Some(command) = command {
+            let text = ratatui::widgets::Paragraph::new(":".to_owned() + command);
+            let mut inner = inner;
+            inner.y = inner.height;
+            inner.height = 1;
+            f.render_widget(text, inner);
+        }
     })?;
 
     Ok(())
